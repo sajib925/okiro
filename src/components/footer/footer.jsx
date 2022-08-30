2f14c475842d2eeb701ed98e748a9676d1ad902b
sajib925 <sajibahmed1004@gmail.com>
2022-08-30 20:59:54 +0600

    create footer section

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,100 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { StyledFooter } from './styles';
+import {BsFacebook} from 'react-icons/bs'
+import {BsTwitter} from "react-icons/bs";
+import {FaMeetup} from "react-icons/fa";
+import {ImYoutube} from "react-icons/im";
+import footerLogo from '../../../public/image/logo/logo.svg'
 
 export const Footer = () => {
   return (
     <StyledFooter>
-        <h1>This is footer</h1>
+      <div className="footerWrap">
+        <div className="footerData">
+          <div className="footerLogo">
+            <img src={footerLogo} alt="footer logo" />
+          </div>
+          <p className="description">
+            A minimal, functional theme for running a paid-membership
+            publication on Ghost.
+          </p>
+          <div className="footerLinks">
+            <Link to="/">
+              <BsFacebook color={"#2b394e"} size={25} />
+            </Link>
+            <Link to="/">
+              <BsTwitter color={"#2b394e"} size={25} />
+            </Link>
+            <Link to="/">
+              <FaMeetup color={"#2b394e"} size={25} />
+            </Link>
+            <Link to="/">
+              <ImYoutube color={"#2b394e"} size={25} />
+            </Link>
+          </div>
+        </div>
+        <div className="footerNav">
+          <div className="footerNavColumn">
+            <small>About</small>
+            <ul>
+              <li>
+                <Link to="/">Features</Link>
+              </li>
+              <li>
+                <Link to="/">Style Guide</Link>
+              </li>
+              <li>
+                <Link to="/">Contact</Link>
+              </li>
+              <li>
+                <Link to="/">Get Theme</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footerNavColumn">
+            <small>FEATURES</small>
+            <ul>
+              <li>
+                <Link to="/">Account</Link>
+              </li>
+              <li>
+                <Link to="/">Membership</Link>
+              </li>
+              <li>
+                <Link to="/">Sign in</Link>
+              </li>
+              <li>
+                <Link to="/">Sign up</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="footerNavColumn">
+            <small>SUPPORT</small>
+            <ul>
+              <li>
+                <Link to="/">Authors</Link>
+              </li>
+              <li>
+                <Link to="/">Tags</Link>
+              </li>
+              <li>
+                <Link to="/">Dark version</Link>
+              </li>
+              <li>
+                <Link to="/">404</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="footerCopyWright">
+        <Link to="/">Okiro </Link>
+        <span to="/">© 2022. All Right Reserved. Published with </span>
+        <Link to="/">Ghost </Link>
+        <span>& </span>
+        <Link to="/">Okiro.</Link>
+      </div>
     </StyledFooter>
-  )
+  );
 }
--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -1,6 +1,79 @@
 import styled from "styled-components";
+import { THEME } from './../../styles/theme';
 
 export const StyledFooter = styled.footer`
-
-
-`
+  padding: 0 80px;
+  margin: 0 311px;
+  .footerWrap {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    grid-column-gap: 81px;
+    .footerData {
+      .footerLogo {
+        margin-bottom: 10px;
+        img {
+          width: 88px;
+          height: 35px;
+        }
+      }
+      .description {
+        font-size: 16px;
+        line-height: 1.4;
+        max-width: 300px;
+        margin: 0 0 4vh;
+        color: ${THEME.colors.primary};
+      }
+      .footerLinks {
+        display: flex;
+        svg {
+          line-height: 1;
+          display: inline-block;
+          width: 40px;
+          height: 40px;
+          margin: 0 7px 7px 0;
+          padding: 0;
+        }
+      }
+    }
+    .footerNav {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      small {
+        font-size: 10px;
+        line-height: 1;
+        display: block;
+        margin: 10px 0 30px;
+        /* padding: 0 20px 0 15%; */
+        letter-spacing: 4px;
+        text-transform: uppercase;
+        font-weight: 800;
+      }
+      ul {
+        padding: 0;
+        li {
+          list-style: none;
+          margin-bottom: 18px;
+          a {
+            text-decoration: none;
+            color: ${THEME.colors.primary};
+            font-size: 16px;
+            font-weight: 900;
+          }
+        }
+      }
+    }
+  }
+  .footerCopyWright {
+    margin: 16px 20px;
+    span {
+      font-size: 12px;
+      color: ${THEME.colors.primary};
+    }
+    a {
+      text-decoration: none;
+      font-size: 12px;
+      color: ${THEME.colors.primary};
+    }
+  }
+`;
